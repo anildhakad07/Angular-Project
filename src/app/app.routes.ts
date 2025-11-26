732279bb4b701a33a1cee30861ca67c56bcd984d
Anil Dhakad <anildhakad8587@gmail.com>
2025-11-26 10:41:54 +0530

    Init Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Members } from './members/members';
+import { SpecialThanks } from './special-thanks/special-thanks';
+import { CoreTeam } from './core-team/core-team';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  { path: 'login', component:Login },
+  { path: 'home', component: Home },
+  { path: 'members', component: Members },
+  { path: 'special-thanks', component: SpecialThanks },
+  { path: 'core-team', component: CoreTeam },
+
+  { path: '**', redirectTo: 'login' },
+];
